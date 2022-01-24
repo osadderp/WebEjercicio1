a9d67a5fd88d50d5c941760a9f92f4ed3b5bb7f8
osadderp <32919721+osadderp@users.noreply.github.com>
2022-01-24 18:44:48 -0500

    Add files via upload

---
+++ WebApiEjercicio1/Controllers/EjercicioDosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+
+namespace WebApiEjercicio1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EjercicioDosController : ControllerBase
+    {
+        Bal.Bal bal;
+        public EjercicioDosController()
+        {
+            bal = new Bal.Bal();
+        }
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<ActionResult<List<int>>> Post([FromBody] RequestP2 request)
+        {
+            List<int> lst;
+            lst = await bal.calcuate2(request);
+            return Ok(lst);
+        }
+    }
+}

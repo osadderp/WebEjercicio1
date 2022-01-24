da1240077222201e92d4686d8d47e9e743716cfb
osadderp <32919721+osadderp@users.noreply.github.com>
2022-01-24 17:32:41 -0500

    Add files via upload

---
+++ Models/Request.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Request
+    {
+        public List<int> lstCasas { get; set; }
+        public int dias { get; set; }
+    }
+}
---
+++ Models/Respone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Respone
+    {
+        public int dias { get; set; }
+        public List<int> entrada { get; set; }
+        public List<int> salida { get; set; }
+    }
+}
